5d74bcec918210c4093e87a85334dbfba1403afa
nicoazzara26 <nicolas.azzara@globallogic.com>
2017-01-07 17:55:20 -0300

    listar archivos drive

--- routes/subjects.js
+++ routes/subjects.js
@@ -1,12 +1,17 @@
 var express = require('express');
-var router = express.Router();
 var google_drive_service = require('../services/google-drive-service');
+var driveFiles = require('../services/quickstart');
+var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  var subjects = google_drive_service.subjects()
+  var subjects = google_drive_service.subjects();
+  var filess = driveFiles.greet();
 
-  res.render('subjectsview', { title: 'Bienvenido a Use the drive' , subjects: subjects});
+    console.log(filess);
+
+
+  res.render('subjectsview', { title: 'Bienvenido a Use the drive' , subjects: subjects, filess:filess});
 });
 
 
